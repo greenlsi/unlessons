b0c4ee8ad967cb506bb96fc9b49e359cccb37634
Jose M. Moya <jose.m.moya@gmail.com>
2015-04-11 09:54:39 +0200

    fix timeval arithmetics

--- reactor/reactor.c
+++ reactor/reactor.c
@@ -4,26 +4,32 @@
 int
 timeval_less (const struct timeval* a, const struct timeval* b)
 {
-  return (a->tv_sec  < b->tv_sec) ||
-    (    (a->tv_sec == b->tv_sec) && (a->tv_usec < b->tv_usec)    );
+  return (a->tv_sec == b->tv_sec)? (a->tv_usec < b->tv_usec) :
+    (a->tv_sec < b->tv_sec);
 }
 
 void
 timeval_add (struct timeval* res,
              const struct timeval* a, const struct timeval* b)
 {
-  res->tv_sec = a->tv_sec + b->tv_sec
-    + a->tv_usec / 1000000 + b->tv_usec / 1000000;
-  res->tv_usec = a->tv_usec % 1000000 + b->tv_usec % 1000000;
+  res->tv_sec = a->tv_sec + b->tv_sec;
+  res->tv_usec = a->tv_usec + b->tv_usec;
+  if (res->tv_usec >= 1000000) {
+    res->tv_sec += res->tv_usec / 1000000;
+    res->tv_usec = res->tv_usec % 1000000;
+  }
 }
 
 void
 timeval_sub (struct timeval* res,
              const struct timeval* a, const struct timeval* b)
 {
-  res->tv_sec = a->tv_sec + a->tv_usec / 1000000
-    - b->tv_sec - b->tv_usec / 1000000 - 1;
-  res->tv_usec = 1000000 + a->tv_usec % 1000000 - b->tv_usec % 1000000;
+  res->tv_sec = a->tv_sec - b->tv_sec;
+  res->tv_usec = a->tv_usec - b->tv_usec;
+  if (res->tv_usec < 0) {
+    res->tv_sec --;
+    res->tv_usec += 1000000;
+  }
 }
 
 
